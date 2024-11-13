0d5151f0f2624d5fcadc8fa23f0903d0a9c1653b
daknine10 <dak910a@gmail.com>
2024-11-13 21:32:20 +0100

    Added hourly section.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./style.css"
 
 
-
+const weatherContainer = document.querySelector(".weather-container")
+const hoursContainer = document.querySelector(".hours")
 const apiKey = "H2XHAPBX54GBKN9P427B5UBWM"
 
 async function getWeather(city) {
@@ -9,35 +10,88 @@ async function getWeather(city) {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`, {mode: 'cors'})
         const weatherData = await response.json();
         console.log(weatherData)
-        let data = [weatherData.resolvedAddress, weatherData.currentConditions.icon, Math.floor(weatherData.currentConditions.temp), weatherData.currentConditions.humidity]
+        let nextHours = []
+        for (let hour of weatherData.days[0].hours) {
+            nextHours.push({
+                datetime: hour.datetime,
+                temp: hour.temp,
+                precipprob: hour.precipprob,
+                icon: hour.icon
+            })
+        }
+        let data = {
+            city: weatherData.resolvedAddress,
+            icon: weatherData.currentConditions.icon, 
+            temp: Math.floor(weatherData.currentConditions.temp),
+            humidity: weatherData.currentConditions.humidity,
+            hours: nextHours
+            }
+        console.log(weatherData.days[0].hours)
         return data
     }
     catch (error) {
-        return console.log("API request unsuccessful")
+        return renderError()
     }
+}
+
+function renderError() {
+    weatherContainer.textContent = "Bad API request"
 }
 
 async function renderWeather(apiCall) {
     const apiReturn = await apiCall
-    const container = document.querySelector(".container")
-    console.log(apiReturn)
 
     const cityName = document.createElement("h2");
-    cityName.textContent = apiReturn[0]
+    cityName.textContent = apiReturn.city
 
     const icon = document.createElement("img")
-    icon.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/58c79610addf3d4d91471abbb95b05e96fb43019/SVG/3rd%20Set%20-%20Color/${apiReturn[1]}.svg`
+    icon.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/58c79610addf3d4d91471abbb95b05e96fb43019/SVG/3rd%20Set%20-%20Color/${apiReturn.icon}.svg`
 
     const temperature = document.createElement("h3");
-    temperature.textContent = apiReturn[2] + ' °C';
+    temperature.textContent = 'Temperature: ' + apiReturn.temp + ' °C';
 
     const humidity = document.createElement("h4");
-    humidity.textContent = apiReturn[3] + '% Humidity';
+    humidity.textContent = apiReturn.humidity + '% Humidity';
 
-    container.append(cityName);
-    container.append(icon);
-    container.append(temperature);
-    container.append(humidity);
+    weatherContainer.append(cityName);
+    weatherContainer.append(icon);
+    weatherContainer.append(temperature);
+    weatherContainer.append(humidity);
 }
 
-renderWeather(getWeather("Wien"))
+async function renderNextHours(apiCall) {
+    const apiReturn = await apiCall
+    console.log(apiReturn.hours)
+    for (let hour of apiReturn.hours) {
+        const hourCard = document.createElement("div");
+        hourCard.className = "hour-card"
+        const time = document.createElement("h3");
+        const icon = document.createElement("img");
+        const temp = document.createElement("h3");
+        const precipprob = document.createElement("h4");
+
+        time.textContent = hour.datetime;
+        icon.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/58c79610addf3d4d91471abbb95b05e96fb43019/SVG/3rd%20Set%20-%20Color/${hour.icon}.svg`
+        temp.textContent = hour.temp + ' °C';
+        precipprob.textContent = 'Rain: ' + hour.precipprob + '%';
+        
+        hoursContainer.append(hourCard)
+        hourCard.append(time)
+        hourCard.append(icon)
+        hourCard.append(temp)
+        hourCard.append(precipprob)
+    }
+}
+
+(function addInput() {
+    const input = document.querySelector("#input")
+    input.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            weatherContainer.textContent = ""
+            hoursContainer.textContent = ""
+            renderWeather(getWeather(`${input.value}`))
+            renderNextHours(getWeather(`${input.value}`))
+            input.value = ""
+        }}
+    )
+})();
